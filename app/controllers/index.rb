8e325f0d6483c5b89a495bafcdca4a35945329b7
Ethan Spiro <ethanspiro@gmail.com>
2014-11-06 17:02:23 -0800

    bunch of shit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -15,14 +15,15 @@ post 'signup' do
 end
 
 post '/login' do
- if User.find_by(:email => params[:email], :password => params[:password])
-  @user = User.find_by(:email => params[:email], :password => params[:password])
-  session[:user_id] = @user.id
-end
-redirect '/'
+  if User.find_by(:email => params[:email], :password => params[:password])
+    @user = User.find_by(:email => params[:email], :password => params[:password])
+    session[:user_id] = @user.id
+  end
+  redirect '/'
 end
 
-get '/survey/:id' do
+
+get '/survey/:id/take' do
   if session[:user_id]
     @user = User.find(session[:user_id])
   end
@@ -30,7 +31,17 @@ get '/survey/:id' do
   erb :single_survey
 end
 
-get '/surveys/:id' do
+post '/survey/:id/take' do
+  #increment choice frequency
+  params[:choices].each do |k, v|
+    temp = Choice.find(v.to_i)
+    temp.choice_frequency += 1
+    temp.save
+  end
+  redirect '/'
+end
+
+get '/:id/surveys' do
  if session[:user_id]
     @user = User.find(session[:user_id])
   end
